5aa5433d6940bede98153c21957089f6e7026ea3
Yi-hao Mo <oldmoh@gmail.com>
2018-01-06 18:02:54 +0800

    Add gaussian blur as pre-processing and morphological opening as post-processing

--- bayes_classifier.py
+++ bayes_classifier.py
@@ -4,11 +4,9 @@ Created on Wed Dec 27 19:22:27 2017
 
 @author: Yi-hao Mo
 """
-import os
 import cv2
 import numpy as np
 import time
-import scipy
 
 
 '''
@@ -17,6 +15,7 @@ labled image has the same shape and pixel depth as those of image
 
 computing log likelihood is easier and we don't need actual value
 we only need to compare the probability of different gaussian distribution
+我已經試過把圖片分成到建築、道路、植被 但是效果非常差
 '''
 
 '''
@@ -27,7 +26,7 @@ curr_time = lambda: int(round(time.time() * 1000))
 '''
     compute mean and covariance
 '''
-def fun(img, label):
+def compute_mean_cov(img, label):
     count = np.count_nonzero(label[:,:,0])
     summation = np.array([0,0,0],dtype = np.int64)
     temp = img * label
@@ -61,47 +60,63 @@ def log_likelihood(x, mean, covariance):
     main process
 '''
 
+#read image and labels
 image_name = "../NEW-AerialImageDataset/AerialImageDataset/train/images/austin1.tif"
 gt_image_name = "../NEW-AerialImageDataset/AerialImageDataset/train/gt/austin1.tif"
-#gt_image_name = "../output/road_label.tif"
+#gt_road = "../output/road_label.tif"
+#gt_plant = "../output/plant_label.tif"
 
 #these two lines are redundant
 source = np.array(cv2.imread(image_name))
 gt_img = np.array(cv2.imread(gt_image_name))
 
-#Pre-process
+#normalizing
+mask = gt_img / 255
+
+'''
+    Pre-processing
+'''
 blurred = cv2.GaussianBlur(source, (5,5), 0)
 image = blurred
 
-# map value of label image to 0~1
-mask = gt_img / 255
+'''
+    training
+'''
+#compute mean and covariance of building and nonbuilding
+mean1, cov1 = compute_mean_cov(source, mask)
+mean2, cov2 = compute_mean_cov(source, 1.0 - mask)
 
 #number of pixel labeled as building
 num_bd_pixel = np.count_nonzero(mask[:,:,0])
 
-# omega 1 is the building set and omega 2 is the nonbuilding set
+# these are ln(P(building)) and ln(P(non-building))
 ln_prob_omega_1 = np.log(num_bd_pixel/(5000*5000))
 ln_prob_omega_2 = np.log(1.0 - num_bd_pixel/(5000*5000))
 
-mean1, cov1 = fun(source,mask)
-mean2, cov2 = fun(source,1.0 - mask)
-
+'''
+    Predicting
+    It takes 2927435 msec = 48.78 min.....QQ
+    
+'''
 start_t = curr_time()
-# labeling
-# take 2927435 msec = 48.78 min.....QQ
-print("labeling")
+print("start predicting")
 ground_truth = np.zeros((5000,5000),dtype=np.uint8)
 for index in np.ndindex(5000,5000):
-        L1 = ln_prob_omega_1 + log_likelihood(image[index], mean1, cov1)
-        L2 = ln_prob_omega_2 + log_likelihood(image[index], mean2, cov2)
-        if L1 > L2:
-            ground_truth[index] = 255
-        
+    L1 = ln_prob_omega_1 + log_likelihood(image[index], mean1, cov1)
+    L2 = ln_prob_omega_2 + log_likelihood(image[index], mean2, cov2)
+    if L1 > L2:
+        ground_truth[index] = 255
         
 print("Execution time: ",end="")
 print(curr_time() - start_t)
 
-cv2.imwrite("../output/ground_truth.tif",ground_truth)
-#cv2.imwrite("../output/road.tif",ground_truth)
+'''
+    Post-processing
+    morphological opening
+'''
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
+result = cv2.morphologyEx(ground_truth, cv2.MORPH_OPEN, kernel)
 
+
+cv2.imwrite("../output/ground_truth.tif",result)
 
